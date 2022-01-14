74e62bfff52eb3edab1297f4d66443cd9c8f4d86
Arjun <arjundoel@gmail.com>
2022-01-14 21:44:44 +0000

    Initial Commit

---
+++ api_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  api_test
+//
+//  Created by Arjun Doel on 14/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ api_test/api_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_testApp.swift
+//  api_test
+//
+//  Created by Arjun Doel on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct api_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
